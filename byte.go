a19c128d7de2381644ee06cc83537472842da162
Mike O <momokatte@hotmail.com>
2017-01-05 15:32:12 -0800

    - removed ByteBucketeer

--- byte.go
+++ byte.go
@@ -6,53 +6,6 @@ import (
 	"github.com/boltdb/bolt"
 )
 
-type ByteBucketeer struct {
-	db   *bolt.DB
-	path Path
-}
-
-func NewByteBucketeer(db *bolt.DB, path Path) (bb *ByteBucketeer) {
-	bb = &ByteBucketeer{
-		db:   db,
-		path: path,
-	}
-	return
-}
-
-func (bb *ByteBucketeer) EnsurePathBuckets() (err error) {
-	err = EnsurePathBuckets(bb.db, bb.path)
-	return
-}
-
-func (bb *ByteBucketeer) EnsureNestedBucket(bucket string) (err error) {
-	err = EnsureNestedBucket(bb.db, bb.path, bucket)
-	return
-}
-
-func (bb *ByteBucketeer) Put(key []byte, value []byte) error {
-	return PutByteValue(bb.db, bb.path, key, value)
-}
-
-func (bb *ByteBucketeer) Get(key []byte) ([]byte, error) {
-	return GetByteValue(bb.db, bb.path, key)
-}
-
-func (bb *ByteBucketeer) Delete(key []byte) error {
-	return DeleteKey(bb.db, bb.path, key)
-}
-
-func (bb *ByteBucketeer) PutNested(bucket string, key []byte, value []byte) error {
-	return PutByteValue(bb.db, bb.path.Nest(bucket), key, value)
-}
-
-func (bb *ByteBucketeer) GetNested(bucket string, key []byte) ([]byte, error) {
-	return GetByteValue(bb.db, bb.path.Nest(bucket), key)
-}
-
-func (bb *ByteBucketeer) DeleteNested(bucket string, key []byte) error {
-	return DeleteKey(bb.db, bb.path.Nest(bucket), key)
-}
-
 func PutByteValue(db *bolt.DB, path Path, key []byte, value []byte) (err error) {
 	txf := func(tx *bolt.Tx) (err error) {
 		var b *bolt.Bucket
